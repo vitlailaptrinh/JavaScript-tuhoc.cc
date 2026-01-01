87b5a1a36ac9d9dae884d7cae42b2dd0725034e8
Nguyen Van B <lethuanh1423@gmail.com>
2026-01-02 00:45:00 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// Hàm hiển thị hộp thoại trên trình
+alert("xin chào");
+// Nhấn Ctrl +/ để tạo ghi chú 1 dòng
+/*
+Ghi chú nhiều dòng
+dòng 1
+dòng 2
+dòng 3
+*/
+
+//Hiển thị hộp thoại nhập liệu
+prompt("Mời nhập vào tên của bạn:");
+
+// Khai báo biến
+// var myNam;
+let yourName;
+// const doSoi = 100;
+
+// Khởi tạo biến
+var diemToan = 10;
+const doSoi = 100;
+let firstName = "Alice";
+
+// Xuất giá trị của biến
+console.log(diemToan);
+console.log("Điểm toán của bạn là: " + diemToan +" điểm");
+
+// Bài tập
+var myName = (prompt("Mời cụ nhập tên:"));
+console.log("Tên của bạn là: " + myName);
+console.log("Chào mừng " + myName +" đến với trang Tuhoc.cc!");
+
