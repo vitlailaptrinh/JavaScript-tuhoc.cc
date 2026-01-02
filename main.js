f439a85b9eb8b00469cb2336d94070d93cdb80a7
Nguyen Van B <lethuanh1423@gmail.com>
2026-01-02 19:23:55 +0700

    4 cách xuất dữ liêu từ biến

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-// tên biến hợp lệ (bắt đầu chữ cái, -, $)
-let name;
-let Name;
-let _name;
-let $name;
-// Các trường hợp không hợp lệ
-//  let 1name;
-//  let @name;
+// 7. Các cách xuất biến dữ liệu với biến trong js
+let soA = 25;
+let soB = 5;
+// Cách 1:
+console.log("Căn bậc hai của " + soA + " là: " +soB);
 
-// let var;
-// let const;
+// Cách 2:
+console.log("Căn bậc hai của", soA, "là:", soB);
 
-// Quy tắc camelCase
-let doSoi;
-let doSoiCuaNuoc;
+// Cách 3:
+console.log(`Căn bậc hai của ${soA} là: ${soB}`);
+
+// Cách 4:
+console.log(`Căn bậc hai của %s là: %s`, soA, soB);
+
+// Luyện tập
+let a = 4,
+    b = 3,
+    kq = a*b;
+
+// Cách 1
+console.log("Kết quả: " + a + " nhân với " + b + " bằng " + kq);
+
+// Cách 2
+console.log("Kết quả:",a,"nhân với",b,"bằng",kq);
+
+// Cách 3
+console.log(`Kết quả: ${a} nhân với ${b} bằng ${kq}`);
+
+// Cách 4
+console.log(`Kết quả: %s nhân với %s bằng %s`, a, b, kq);
